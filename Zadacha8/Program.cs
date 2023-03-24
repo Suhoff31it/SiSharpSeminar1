7645ca25c26f0ab810bb8841cfe4aa2573719cc6
Alexs <suhoff31it@gmail.com>
2023-03-24 19:10:08 +0300

    поиск четных чисел в диапазоне от 1 до N

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,11 @@
+﻿// See Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.WriteLine("Ведите число N");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= number; i++)
+{
+    if (i % 2 == 0)
+        System.Console.Write(i + "," );
+}
